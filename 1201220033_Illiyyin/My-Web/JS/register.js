7f4ceb529a900318f10179b094678aab45ab7eac
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2023-11-01 19:38:49 +0700

    Test code, implemented DOM.

--- 1201220033_Illiyyin/My-Web/JS/register.js
+++ 1201220033_Illiyyin/My-Web/JS/register.js
@@ -1,1 +1,17 @@
-alert("Hello World");
+function HelloWorld() {
+    alert("Hello World");
+}
+
+window.HelloWorld();
+
+document.body.style.backgroundColor = "red";
+setTimeout(() => document.body.style.backgroundColor = "", 5000);
+
+
+function TestLoop() {
+    for (let node of document.body.childNodes) {
+        alert(node);
+    }
+}
+
+TestLoop();
