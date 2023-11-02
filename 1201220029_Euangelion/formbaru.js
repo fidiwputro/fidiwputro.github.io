9ce99626eeb0e9665f303d32f2bd802cdb9c6884
unknown <michdidimus@gmail.com>
2023-11-02 13:07:15 +0700

    .

---
+++ 1201220029_Euangelion/formbaru.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",(event)=>{
+    let form = document.forms[0];// DOM
+    form.addEventListener("submit",(evt)=>){
+        evt.preventDefault();//mencegah submit
+        let data = new 
+        console.log ('tes submit')
+    }
+}
+console.log("tes");
