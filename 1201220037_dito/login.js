058ddba78d6f4ba50265b9e83f4b1f285a92f746
Dito-7 <112498814+Dito-7@users.noreply.github.com>
2023-11-06 19:45:22 +0700

    update

---
+++ 1201220037_dito/login.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    // document.getElementById('username').style.border = "1px solid red";
+    let form = document.forms[0];//DOM
+
+    form.addEventListener("submit", (evt) => {
+        // alert('submit');
+        evt.preventDefault();
+
+        let dataa = new FormData(form);//Object Form Data
+        let Error = "Please enter your";
+        let ValidasiSubmit = true;
+
+        dataa.forEach((isi, nama) => {
+            console.log(nama + " = " + isi);
+
+            if (isi == '' || isi == 0) {
+                form[nama].style.border = "1px solid red";
+                form[nama].style.transition = "0.5s ease";
+                form[nama].label = nama;
+
+                form[nama].setAttribute('placeholder', Error + ' ' + nama);
+                form[nama].style.transition = "0.5s ease";
+                form[nama].classList.add('placeholderMerah');
+
+                ValidasiSubmit = false;
+            } else {
+                form[nama].style.border = "none";
+                form[nama].style.transition = "0.5s ease";
+                form[nama].setAttribute('placeholder', nama);
+
+                // alert(nama + ' : ' + isi);
+            }
+        });
+
+        if (ValidasiSubmit) {
+            form.submit();
+        }
+
+    });
+
+});
+
+console.log("tes");
