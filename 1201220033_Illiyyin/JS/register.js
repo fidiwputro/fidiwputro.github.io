3e177bbe2edaa6043ba2c24004abe2639e222407
Illiyyin Akbar Ariyanto <illiyyin.akbar123@gmail.com>
2023-11-08 15:25:35 +0700

    Add function 'MustFillForm' to fill form, if not it send alert message to fill form.

--- 1201220033_Illiyyin/JS/register.js
+++ 1201220033_Illiyyin/JS/register.js
@@ -6,13 +6,41 @@ function InputRegistration() {
     let phone_number = document.getElementById("phone_number").value;
 
     if (full_name && username && password && email && phone_number){
-        alert(`full_name: ${full_name}\nusername: ${username}\npassword: ${password}\nemail: ${email}\nphone_number: ${phone_number}`)
+        // alert(`full_name: ${full_name}\nusername: ${username}\npassword: ${password}\nemail: ${email}\nphone_number: ${phone_number}`)
+        alert("Form Submited");
         return;
     }else{
-        alert("Please fill in the form correctly!")
+        // alert("Please fill in the form correctly!")
+        MustFillForm();
         // This line of code prevents the form from being submitted if the username or password fields are empty.
         event.preventDefault();
         return;
+    }
+}
+
+function MustFillForm() {
+    let full_name = document.getElementById("full_name");
+    let username = document.getElementById("username");
+    let password = document.getElementById("password");
+    let email = document.getElementById("email");
+    let phone_number = document.getElementById("phone_number");
+
+    let fields = [full_name, username, password, email, phone_number];
+    let fieldNames = ["full name", "username", "password", "email", "phone number"];
+
+    let isFormFilled = true;
+
+    for (let i = 0; i < fields.length; i++) {
+        if (!fields[i].value) {
+            alert(`Please fill in the ${fieldNames[i]} correctly!`);
+            fields[i].focus();
+            fields[i].style.backgroundColor = "red";
+            isFormFilled = false;
+        }
+    }
+
+    if (isFormFilled) {
+        alert("Form Submitted");
     }
 }
 document.getElementById("submit").addEventListener("click", InputRegistration)
