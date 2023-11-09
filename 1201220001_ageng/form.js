320a6d97e4454ee3a207ace3b9ed312278071633
unknown <theqar07@gmail.com>
2023-11-09 13:21:14 +0700

    .

--- 1201220001_ageng/form.js
+++ 1201220001_ageng/form.js
@@ -4,23 +4,6 @@ document.addEventListener("DOMContentLoaded", (event)=>{
     form.addEventListener("submit", (evt)=>{
         evt.preventDefault();
         let data = new FormData(form); // object Form Data
-        // console.log([...data.entries()]);
-        // data.forEach((val,key)=>{
-        //     console.log(key + " " + val);
-        //     if (val=='' || val==0){
-        //         alert(key + ' tidak boleh kosong');
-        //         // form[key].style.backgroundColor = 'pink';
-        //     } else if (val){
-        //         alert(key + ' Terisi');
-        //     }
-        // });
         console.log(...data.entries());
-        // data.append('kota','Wonogiri');
-        // console.log(...data.entries());
-        // data.delete('txtnama');
-        // console.log(...data.entries());
-        // console.log(data.get('txtnama'));
-        console.log(data.has('txtnama'));
-
     })
 });
