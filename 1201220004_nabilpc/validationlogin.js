8b38369046379e656cb2ba4049f793b26d3acc21
BaksoRadja <idbaksoradja@gmail.com>
2023-11-09 10:51:46 +0700

    Add effect if form kosong

--- 1201220004_nabilpc/validationlogin.js
+++ 1201220004_nabilpc/validationlogin.js
@@ -1,9 +1,13 @@
 function notifikasi() {
     let uname = document.getElementById('username');
     let pass = document.getElementById('password');
-    if (uname.value.trim() == '' || pass.value.trim() == '') {
-        alert('Username dan Password tidak boleh kosong :|');
-    } else {
+    if (uname.value.trim() == '') {
+        alert('Username tidak boleh kosong :|');
+        uname.style.backgroundColor = 'pink';
+    }else if (pass.value.trim() == ''){
+        alert('Password tidak boleh kosong :|');
+        pass.style.backgroundColor = 'pink';
+    }else {
         alert('halo, ' + uname.value);
     }
 }
