41d1970d949e79b083ac9465a9c511d022522465
BaksoRadja <idbaksoradja@gmail.com>
2023-11-09 10:51:19 +0700

    Add effect if form kosong

--- 1201220004_nabilpc/validationregister.js
+++ 1201220004_nabilpc/validationregister.js
@@ -6,9 +6,26 @@ function notifikasi() {
     let username =document.getElementById('username');
     let password =document.getElementById('password');
     let confirmpassword =document.getElementById('confirmpass');
-    if (fname.value.trim() == '' || password.value.trim() == '' || lname.value.trim() == '' || email.value.trim() == '' || username.value.trim() == '' || confirmpassword.value.trim() == '') {
+    if (fname.value.trim() == '') {
         alert('Data tidak boleh ada yang kosong :|');
+        fname.style.backgroundColor = 'pink';
+    }else if (password.value.trim() == '') {
+        alert('Data tidak boleh ada yang kosong :|');
+        password.style.backgroundColor = 'pink';
+    }else if (lname.value.trim() == '') {
+        alert('Data tidak boleh ada yang kosong :|');
+        lname.style.backgroundColor = 'pink';
+    }else if (email.value.trim() == '') {
+        alert('Data tidak boleh ada yang kosong :|');
+        email.style.backgroundColor = 'pink';
+    }else if (username.value.trim() == '') {
+        alert('Data tidak boleh ada yang kosong :|');
+        username.style.backgroundColor = 'pink';
+    }else if (confirmpassword.value.trim() == '') {
+        alert('Data tidak boleh ada yang kosong :|');
+        confirmpassword.style.backgroundColor = 'pink';
     }else if (password.value != confirmpassword.value) {
+        confirmpassword.style.backgroundColor = 'pink';
         alert('Konfirmasi Password Tidak Sama dengan Password!');
     }else {
         alert('halo, ' + username.value);
