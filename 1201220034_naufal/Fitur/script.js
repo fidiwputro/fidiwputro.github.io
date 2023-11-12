332d4210130051402a4b581e92dc751cb0eb860f
Naufal453 <105023440+Naufal453@users.noreply.github.com>
2023-11-12 10:49:32 +0700

    Update script.js

--- 1201220034_naufal/Fitur/script.js
+++ 1201220034_naufal/Fitur/script.js
@@ -1,15 +1,19 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
   const registrasiForm = document.querySelector(".Registrasi form");
+  const loginForm = document.querySelector(".Login form");
   const usernameR = document.querySelector(".UsernameR");
   const passwordR = document.querySelector(".PasswordR");
   const konfirmasiR = document.querySelector(".KonfirmasiR");
+  const usernameL = document.querySelector(".UsernameL");
+  const passwordL = document.querySelector(".PasswordL");
   const registeredMessage = document.querySelector("#registered");
+  const loginMessage = document.querySelector("#loginMessage");
 
   registrasiForm.addEventListener("submit", function (e) {
     e.preventDefault();
     
-    // Validasi input
+    // Validasi input registrasi
     if (usernameR.value.trim() === "") {
       registeredMessage.innerText = "Username harus diisi.";
       return;
@@ -26,5 +30,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Set pesan sukses
     registeredMessage.innerText = "Registrasi berhasil!";
+  });
+
+  loginForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // Validasi input login
+    if (usernameL.value.trim() === "") {
+      loginMessage.innerText = "Username harus diisi.";
+      return;
+    }else if (passwordL.value.trim() === "") {
+      loginMessage.innerText = "Password harus diisi.";
+      return;
+    }
+
+    // Jika semua input login valid, Anda dapat menambahkan kode untuk melakukan autentikasi sesuai kebutuhan.
+    // Misalnya, memeriksa username dan password di database.
+
+    // Set pesan sukses
+    loginMessage.innerText = "Login berhasil!";
   });
 });
